0a6f90334599831f1874c806e8d50a79fc4f0596
Lucas Nestler <lucasnestler@web.de>
2020-03-26 19:00:38 +0100

    feat(network): add handling for replies

--- networking.py
+++ networking.py
@@ -8,12 +8,16 @@ from config import P2P_PORT
 REQUEST_TO_FUNCTION = {'read_block':       interface.read_block,
                        'read_transaction': interface.read_transaction,
                        'add_transaction':  interface.store_transaction,
-                       'add_block':        interface.store_block
+                       'add_block':        interface.store_block,
+                       'reply':            None
                        }
 
 
 def init_node():
     running = True
+
+    request_to_function = REQUEST_TO_FUNCTION.copy()
+    request_to_function['reply'] = interface.mailbox_handler(mailbox)
 
     def start_node(port):
         node = net.Net(passive_port=port, node_type='passive')
@@ -31,7 +35,9 @@ def init_node():
                 for reply in connection:
                     try:
                         function = REQUEST_TO_FUNCTION.get(reply.pop('request_type'))
-                        function(**reply)
+                        answer = function(**reply)
+                        if answer is not False:
+                            connection.send({'request_type': 'reply', 'data': answer})
                     except Exception as exc:
                         print(f'{exc.__class__.__name__} occured with message "{exc}"')
                         continue
