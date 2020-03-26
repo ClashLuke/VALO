71f6be0ceda484a2c195736db4cf62d9aa039e75
Lucas Nestler <lucasnestler@web.de>
2020-03-26 02:55:47 +0100

    feat(datatypes): improve verification depth of transaction

--- datatypes.py
+++ datatypes.py
@@ -1,5 +1,13 @@
+import pickle
+import random
+import sys
+import time
+
+import config
 import crypto
 import database
+import utils
+import interface
 
 
 def transaction(wallet_in: str, wallet_out: str, amount: int, index: int,
@@ -8,21 +16,28 @@ def transaction(wallet_in: str, wallet_out: str, amount: int, index: int,
                         'amount':    amount, 'index': index
                         }
     signer, verifier, _ = crypto.eddsa(wallet_in, private_key)
-
-    def verify(signature: bytes):
-        return verifier(crypto.pickle_hash(transaction_dict), signature)
+    validated = [None]
+    transaction_hash = [None]
 
     def sign():
         return signer(crypto.pickle_hash(transaction_dict))
 
-    def store(signature: bytes):
-        assert verify(signature)
-        assert database.read('wallet', wallet_in) >= amount
-        transaction_hash = crypto.pickle_hash(transaction_dict)
-        assert database.read('transaction', transaction_hash) is None
+    def verify(signature: bytes):
+        if not verifier(crypto.pickle_hash(transaction_dict), signature):
+            return False
+        if not database.read('wallet', wallet_in) >= amount:
+            return False
+        tx_hash = crypto.pickle_hash(transaction_dict)
+        if database.read('transaction', tx_hash) is not None:
+            return False
+        validated[0] = True
+        transaction_hash[0] = tx_hash
+        return True
 
-        database.write(transaction_dict, 'transaction', transaction_hash)
-        database.sub('wallet', wallet_in, amount)
-        database.add('wallet', wallet_out, amount)
+    def store(signature: bytes):
+        if validated[0] or (validated[0] is None and verify(signature)):
+            database.write(transaction_dict, 'transaction', transaction_hash[0])
+            database.sub('wallet', wallet_in, amount)
+            database.add('wallet', wallet_out, amount)
 
     return sign, verify, store
