2991495518227f84371e06d3ed6f634068da97d8
Lucas Nestler <lucasnestler@web.de>
2020-03-26 02:56:02 +0100

    feat(datatypes): add block

--- datatypes.py
+++ datatypes.py
@@ -41,3 +41,68 @@ def transaction(wallet_in: str, wallet_out: str, amount: int, index: int,
             database.add('wallet', wallet_out, amount)
 
     return sign, verify, store
+
+
+def block(block_index, wallet, transactions, difficulty, timestamp=None, nonce=None,
+          signature=None, private_key=None):
+    header = {'wallet':     wallet, 'transactions': transactions, 'nonce': nonce,
+              'timestamp':  int(time.time()) if timestamp is None else timestamp,
+              'difficulty': difficulty, 'block_index': block_index,
+              'signature':  signature
+              }
+
+    signer, verifier, _ = crypto.eddsa(wallet, private_key)
+
+    def sign():
+        header['signature'] = None
+        header['signature'] = signer(crypto.pickle_hash(header))
+
+    if signature is None:
+        sign()
+
+    diff = 2 ** 512 - 1
+    diff //= difficulty
+
+    def check_hash(header_hash):
+        return utils.bytes_to_int(header_hash) < difficulty
+
+    def random_hash():
+        header['nonce'] = random.randint(0, 2 ** 512)
+        sign()
+        return crypto.pickle_hash(header)
+
+    def mine():
+        header_hash = random_hash()
+        while check_hash(header_hash):
+            header_hash = random_hash()
+        return header
+
+    def verify():
+        block_hash = check_hash(crypto.pickle_hash(header))
+        if not check_hash(block_hash):
+            return False
+        header['signature'] = None
+        if not verifier(crypto.pickle_hash(header), signature):
+            header['signature'] = signature
+            return False
+        if database.read('connection+block_index+block', block_index) is not None:
+            return False
+        header['signature'] = signature
+        for i in range(transactions):
+            transactions[i] = transaction(**transactions[i])
+            if not transactions[i][0]():
+                return False
+        return True
+
+    def store():
+        if verify():
+            database.write(header, 'block', crypto.pickle_hash(header))
+            for tx in transactions:
+                tx[2]()
+            block_size = sys.getsizeof(pickle.dumps(header, protocol=4))
+            old_mean = interface.add_mean_block_size(block_size)
+            reward = config.reward_function(block_index, block_size, old_mean)
+            database.add('wallet', wallet, reward)
+            database.add('block_height', 'main', 1)
+
+    return check_hash, mine, verify, store
