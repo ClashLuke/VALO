40d3f4a486c7823f2aaff5dd43ac2d7308a957db
Lucas Nestler <lucasnestler@web.de>
2020-03-27 19:26:19 +0100

    style(config): add missing newline

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ UNIT = 10 ** 6
 BLOCK_TIME = 120
 LWMA_WINDOW = 90
 
+
 def reward_function(block_index, block_size, old_mean):
     size_factor = block_size / min(1, abs(block_size - old_mean)) ** 1.5
     return size_factor * UNIT / math.log2(2 + block_index)
