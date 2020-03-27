1fdf437831c5e97aee20cb740df6a12e16bc6571
Lucas Nestler <lucasnestler@web.de>
2020-03-27 16:25:56 +0100

    fix(config): reduce aggressiveness of old mean

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ UNIT = 10 ** 6
 
 
 def reward_function(block_index, block_size, old_mean):
-    size_factor = block_size / (block_size - old_mean) ** 2
+    size_factor = block_size / min(1, abs(block_size - old_mean)) ** 1.5
     return size_factor * UNIT / math.log2(2 + block_index)
