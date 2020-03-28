02b5e0db4882c93f66689eeb8da5605030620fbb
Lucas Nestler <lucasnestler@web.de>
2020-03-28 02:03:51 +0100

    fix(crypto): replace ecpy with nacl

--- crypto.py
+++ crypto.py
@@ -1,31 +1,28 @@
 import pickle
-from hashlib import sha256 as hash_function
-from random import randint
 
-from ecpy.curves import Curve
-from ecpy.eddsa import ECPrivateKey, EDDSA
-
-
-CRYPTO = EDDSA(hash_function)
-CURVE = Curve.get_curve('Ed25519')
+from nacl import encoding, signing
+from nacl.hashlib import blake2b as hash_function
 
 
 def eddsa(public=None, private=None):
     if public is None:
-        private = ECPrivateKey(randint(0, 2 ** 256), CURVE)
-        public = private.get_public_key()
+        private = signing.SigningKey.generate()
+        public = private.verify_key
+    elif not isinstance(public, signing.VerifyKey):
+        public = signing.VerifyKey(public.encode(), encoder=encoding.Base32Encoder)
 
     def sign(msg):
-        return CRYPTO.sign(msg, private)
+        sig = private.sign(msg).signature
+        return sig
 
     def keys():
-        return public, private
+        return public.encode(encoder=encoding.Base32Encoder).decode(), private
 
     def verify(msg, sig):
-        return CRYPTO.verify(msg, sig, public)
+        return public.verify(msg, sig)
 
     return sign, verify, keys
 
 
 def pickle_hash(obj):
-    return hash_function(pickle.dumps(obj, protocol=4)).digest()
+    return hash_function(pickle.dumps(obj)).digest()
