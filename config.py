d4124d3a3ca0d6596b4a96d1d9fe1f456223ae83
Lucas Nestler <lucasnestler@web.de>
2020-03-28 03:32:04 +0100

    feat(config): add reduction factor to reward equation

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-import math
-
 P2P_PORT = 60337
 RPC_PORT = 61337
 
@@ -8,9 +6,12 @@ UNIT = 10 ** 6
 BLOCK_TIME = 120
 LWMA_WINDOW = 90
 
+REDUCTION_FACTOR = 2 ** 20
+
 SEEDS = [{"addr": "127.0.0.1", "port": 8000}]
 
 
 def reward_function(block_index, block_size, old_mean):
     size_factor = block_size / min(1, abs(block_size - old_mean)) ** 1.5
-    return size_factor * UNIT / math.log2(2 + int(block_index))
+    index_reduction = (1 - 1 / REDUCTION_FACTOR) ** int(block_index)
+    return size_factor * index_reduction * UNIT
