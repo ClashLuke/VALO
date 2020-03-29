a2284d720927598ffe0fc11055ac6f58d0997500
Lucas Nestler <lucasnestler@web.de>
2020-03-29 21:30:34 +0200

    fix(config): decode genesis hash

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ GENESIS_HASH = base64.b64decode(
         "/OzZD4mRLuzMvnKqUwHO1+WA7I6+CO8b+3AHNfkDx725tunk+TxgUUFjDkRmhdwStoR+9JvL6L"
         "/0kFOesF4WBS6Bwi7bdDNFB5TfWprlS4bZG/oYsPJjxBGUlwO3EWulM6zjt0H3TOk0DwKze8eNQfB6"
         "/mpTe0VHP39+vhFXw2d8ttxkfdRadr0iLFeK2QZ/K8DPEJxZ9qYg6aSQ"
-        "+kBlaTw3CawyiMSPWKVtcSRW7iGEIogbViGx4PvdwG38wAx/D/qR+78qKybMA==")
+        "+kBlaTw3CawyiMSPWKVtcSRW7iGEIogbViGx4PvdwG38wAx/D/qR+78qKybMA=="
+        ).decode(errors='ignore')
 
 
 def reward_function(block_index, block_size, old_mean):
