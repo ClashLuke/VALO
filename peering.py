a2d0450a7d2bf96650aebf670ecc7e1967e73417
Lucas Nestler <lucasnestler@web.de>
2020-03-30 05:25:19 +0200

    feat(peering): add comparison function

--- peering.py
+++ peering.py
@@ -19,9 +19,33 @@ class Peer:
     def __init__(self, ip, port, log_file='p2p.log'):
         self.ip = ip
         self.port = port
-        self.routes = {}
-        self.known_ips = set()
+        self.routes = {'compare': self.compare_listener}
+        self.iterator = {}
         self.log_file = open(log_file, 'a')
+        self.connection = None
+
+    def compare_listener(self, init_item, iterator_name):
+        connection_item = init_item
+        iterator = self.iterator[iterator_name]
+        for item in iterator:
+            self.connection.sendall(bytes(int(connection_item == item)))
+            connection_item = receive_all(self.connection)
+            if connection_item is None:
+                break
+        return None
+
+    def compare(self, target, iterator, iterator_name, ip):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((ip, self.port))
+            sock.sendall(utils.dumps({'request_type':  'compare',
+                                      'init_item':     next(iterator),
+                                      'iterator_name': iterator_name
+                                      }))
+            for i, item in enumerate(iterator):
+                sock.sendall(item)
+                if int.from_bytes(receive_all(sock), 'little') != target:
+                    break
+        return i
 
     def listen(self, active_connections: set = None):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -30,12 +54,13 @@ class Peer:
             while True:
                 try:
                     connection, (host_ip, _) = sock.accept()
-                    self.known_ips.add(host_ip)
                     result = receive_all(connection)
+                    self.connection = connection
                     request_type = result.pop('request_type')
                     value = self.routes[request_type](**result)
-                    value = utils.dumps(value)
-                    connection.sendall(value)
+                    if value is not None:
+                        value = utils.dumps(value)
+                        connection.sendall(value)
                     if active_connections is not None:
                         active_connections.add(host_ip)
                 except Exception as exc:
